5a91eed86e2766400b761016fd9b6a682a95c98f
Vítor Cézar <vitorcezli@gmail.com>
2019-08-17 17:34:17 -0300

    Changes.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from wiotp import sdk
 import json
 import numpy as np
 import io
+from watson_machine_learning_client import WatsonMachineLearningAPIClient
 from PIL import Image
 
 app = Flask(__name__)
@@ -75,9 +76,23 @@ def predict():
     image = prepare_image(image)
 
     # Faça uma requisição para o serviço Watson Machine Learning aqui e retorne a classe detectada na variável 'resposta'
-    
+    wml_credentials  = {
+        "apikey": "w0Jfv25Y1emvcA6aA-Gf_Eys9ZzPpHj0EQdASS3Kbldz",
+        "iam_apikey_description": "Auto-generated for key b1e50421-929e-4d18-98e9-32729c898390",
+        "iam_apikey_name": "wdp-writer",
+        "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
+        "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/c803aecf7aac4d009194a0cce5abb600::serviceid:ServiceId-7cdc734b-12d3-4b8c-90fd-8ac4d377d601",
+        "instance_id": "bdd4074e-e090-4d18-998d-c2cf18a1f3e6",
+        "url": "https://us-south.ml.cloud.ibm.com"
+    }
+    client = WatsonMachineLearningAPIClient( wml_credentials )
+    model_payload = { "values" : image }
+    ai_parms = { "wml_credentials" : wml_credentials, "model_endpoint_url" : 'https://us-south.ml.cloud.ibm.com/v3/wml_instances/bdd4074e-e090-4d18-998d-c2cf18a1f3e6/deployments/8165db11-40d7-438b-ac99-714156339e5f/online' }
+    model_result = client.deployments.score( ai_parms["model_endpoint_url"], model_payload )
+
+
     resposta = {
-        "class": "data"
+        "class": 'DIRTY' if model_result['values'][0][1][0] == 1 else 'CLEAN'
     }
     return resposta
 
